577746556dc0c8ad29b7955321e9301f73d8984c
Artur Shamsutdinov <a.shamsutdinov@icloud.com>
2021-04-24 20:16:40 +0500

    First level edited

--- Player.gd
+++ Player.gd
@@ -1,7 +1,5 @@
 extends KinematicBody
 
-onready var TweenNode = get_node("Tween")
-
 const MOVE_SPEED = 4
 const MOUSE_SENS = 0.5
 
@@ -23,18 +21,3 @@ func _physics_process(delta):
 	move_vec = move_vec.normalized()
 	move_vec = move_vec.rotated(Vector3(0, 1, 0), rotation.y)
 	move_and_collide(move_vec * MOVE_SPEED * delta)
-	
-#	TweenNode.interpolate_method(self, "process_move", transform, move_vec, 2.0, Tween.TRANS_LINEAR, Tween.EASE_IN)
-#	TweenNode.start()
-		
-	
-#func process_move():
-#	move_vec = move_vec.normalized()
-#	move_vec = move_vec.rotated(Vector3(0, 1, 0), rotation.y)
-#	move_and_collide(move_vec * MOVE_SPEED * DELTA2)
-	
-	#TweenNode.interpolate_property(self, "translation", 0, 5, 2.0, Tween.TRANS_LINEAR, Tween.EASE_IN)
-	#TweenNode.start()
-
-
-
